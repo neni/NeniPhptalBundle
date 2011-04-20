2a8153c62217899ee52bbea236266222dc58ff09
Stéphane Louédin <dev@neni.org>
2011-04-20 16:29:07 +0200

    add global variables to the default populater

--- Phptal/PhptalPopulater.php
+++ Phptal/PhptalPopulater.php
@@ -4,7 +4,7 @@ namespace Neni\PhptalBundle\Phptal;
 
 use Neni\PhptalBundle\Phptal\PhptalPopulaterInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-
+use Symfony\Bundle\FrameworkBundle\Templating\GlobalVariables;
 
 // TODO: add globals and other things
 
@@ -12,16 +12,18 @@ class PhptalPopulater implements PhptalPopulaterInterface
 {
 
     protected $container;
-    
+    protected $globals;
 
-    public function __construct(ContainerInterface $container)
+    public function __construct(ContainerInterface $container, GlobalVariables $globals)
     {
         $this->container = $container;
+        $this->globals = $globals;
     }
 
 
     public function populate($template, $parameters)
     {
+        $template->app = $this->globals;
         foreach ($parameters as $k=>$v){
             $template->$k = $v;
         }
